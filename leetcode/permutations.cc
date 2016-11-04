0f45313413e636ae5e850647ddcf44130d7260ba
Potato <twelendil@gmail.com>
2016-11-04 09:50:15 +0800

    leetcode/permutations.cc

---
+++ leetcode/permutations.cc
@@ -0,0 +1,22 @@
+#include <algorithm>
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        dfs(nums, 0, result);
+        return result;
+    }
+    
+    void dfs(vector<int>& nums, int pos, vector<vector<int>>& result) {
+        if (pos >= nums.size()) {
+            result.push_back(nums);
+        } else {
+            for (int i = pos; i < nums.size(); i += 1) {
+                swap(nums[i], nums[pos]);
+                dfs(nums, pos + 1, result);
+                swap(nums[i], nums[pos]);
+            }
+        }
+    }
+};
