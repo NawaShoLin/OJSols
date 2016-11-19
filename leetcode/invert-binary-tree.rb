b5c2e564b1ec45c208910ff7986726c58fb176fb
Potato <twelendil@gmail.com>
2016-11-19 20:56:03 +0800

    leetcode/invert-binary-tree.rb

---
+++ leetcode/invert-binary-tree.rb
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {TreeNode}
+
+def invert_tree(root)
+    return nil if root == nil
+    
+    root.left, root.right = root.right, root.left
+    
+    invert_tree(root.left)
+    invert_tree(root.right)
+    
+    root
+end
