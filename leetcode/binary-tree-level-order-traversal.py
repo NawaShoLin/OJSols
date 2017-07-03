9c51b4c9ceb2ab3a2ba4438e05d6326d503d556c
Potato <twelendil@gmail.com>
2017-07-03 14:56:26 +0800

    leetcode/binary-tree-level-order-traversal.py

---
+++ leetcode/binary-tree-level-order-traversal.py
@@ -0,0 +1,22 @@
+import Queue
+
+class Solution(object):
+    def levelOrder(self, root):
+        if root is None:
+            return []
+        
+        container = []
+        q = Queue.Queue()
+        q.put([0, root])
+        
+        while not q.empty():
+            lv, node = q.get()
+            if len(container) - 1 < lv:
+                container.append([])
+            container[lv].append(node.val)
+            
+            for child in [node.left, node.right]:
+            	if child is not None:
+                	q.put([lv + 1, child])
+                
+        return container
